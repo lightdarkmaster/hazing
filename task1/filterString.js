f60c9a072b783971c6218f196aedf9134e0d6e98
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-03-14 11:12:56 +0800

    Add Solution For Number 5

--- task1/filterString.js
+++
@@ -1,9 +0,0 @@
-console.log("Test I: Create a function that filters an array of strings based on the length of each string, returning only strings longer than a specified length.");
-
-const test1 = (array, length) => {
-
-    const result = array.filter((array) => array.length > length);
-    return result;
-}
-
-console.log(test1(["Apple", "Banana", "Peaches", "Mangoes", "Absdedede", "Apt"], 6));
