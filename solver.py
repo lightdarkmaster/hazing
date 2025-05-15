85f95f7d8ebfb8015e47a7f305fb97acdee71885
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-05-15 13:22:19 +0800

    Update Codebase

--- solver.py
+++ solver.py
@@ -4,12 +4,12 @@ import datetime
 totalHoursNeeded = 486;
 
 print("OJT Hour Solver");
-print("Note: Assuming that you will render 8 hours per day and no absence starting today. ");
-hoursRendered = int(input(f"Enter Rendered Hours: "))
+print("Note: Assuming that you will render 8 hours per day and \n no absence starting today. ");
+hoursRendered = int(input(f"Enter Rendered Hours: "));
 remainingHours = totalHoursNeeded - hoursRendered;
 print("Remaining Hours: " + str(remainingHours) + " Hours");
 remainingDays = round(remainingHours / 8);
 print("Remaining Days: " + str(remainingDays) + " Days");
 
-endDate = datetime.date.today() + datetime.timedelta(days=round(remainingHours / 8))
-print("End Date: " + endDate.strftime("%B %d, %Y"))
+endDate = datetime.date.today() + datetime.timedelta(days=round(remainingHours / 8));
+print("End Date: " + endDate.strftime("%B %d, %Y"));
