d0a27f55f3f138767fccaae22468fce5e9a480f2
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-02-02 16:00:28 +0800

    POC date config for the widget

--- date.js
+++ date.js
@@ -1,3 +1,15 @@
 const getDate = () => new Date().toISOString().slice(0, 10);
 
 console.log(getDate());
+
+const currentYear = new Date().getFullYear();
+console.log(currentYear.toString());
+
+const getCurrentMonth = new Date().getMonth().toString();
+console.log(getCurrentMonth);
+
+const getDateSum = (string) =>{
+    console.log(string);
+}
+
+console.log(getDateSum(20240610));
