aa4e66240c2bc63ea46892f64c20dcc1c9d24e49
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-08-08 08:34:09 +0800

    Add TestExam

---
+++ TestExam/testExam1.js
@@ -0,0 +1,28 @@
+const word = "Eagle";
+const wordToLowerCase = word.toLowerCase();
+const vowels = ["a", "e", "i", "o", "u"];
+
+for(const vowel of vowels){
+    // console.log(vowel);
+}
+
+// if(word.length > 0){
+//     if(word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u") || word.startsWith("A") || word.startsWith("E") || word.startsWith("I") || word.startsWith("O") || word.startsWith("U")){
+//         console.log(word + "yay");
+//     }else{
+//         console.log(word + "ay");
+//     }
+// }else{
+//     console.log("There is no similar letter");
+// }
+
+
+if(wordToLowerCase.length > 0){
+    if(wordToLowerCase.startsWith(vowels[vowels.indexOf(wordToLowerCase[0])])){
+        console.log(word + "yay");
+    }else{
+        console.log(word + "ay");
+    }
+}else{
+    console.log("Invalid Input, Reason only 1 letter or numbers inputed or empty string");
+}
