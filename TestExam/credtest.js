fd684579c3500546113a67fc361070896067e976
lightdarkmaster <christian.barbosa05222001@gmail.com>
2026-09-18 14:48:56 +0800

    Add expense

---
+++ TestExam/credtest.js
@@ -0,0 +1,13 @@
+console.log("Test Credentials");
+
+const username = "testuser";
+const password = "testpassword";
+
+
+
+
+if (username === "testuser" && password === "testpassword") {
+    console.log("Login successful");
+} else {
+    console.log("Invalid credentials");
+}
